cea4b7e64c597aa6a0e1c4fba34041efe4c3a274
ShowravKormokar <showravkormokar354@gmail.com>
2026-03-24 23:40:29 +0600

    Solved: Leetcode problem 2906 ->. Construct Product Matrix

---
+++ Arrays/MediumQusSolve/constructProductMtx.cpp
@@ -0,0 +1,51 @@
+// Leetcode problem 2906 ->. Construct Product Matrix
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<vector<int>> constructProductMatrix(vector<vector<int>> &grid)
+    {
+        constexpr int kMod = 12345;
+        const int m = grid.size();
+        const int n = grid[0].size();
+        vector<vector<int>> ans(m, vector<int>(n));
+        vector<int> prefix{1};
+        int suffix = 1;
+
+        for (const vector<int> &row : grid)
+            for (const int num : row)
+                prefix.push_back(static_cast<long>(prefix.back()) * num % kMod);
+
+        for (int i = m - 1; i >= 0; --i)
+            for (int j = n - 1; j >= 0; --j)
+            {
+                ans[i][j] = prefix[i * n + j] * suffix % kMod;
+                suffix = static_cast<long>(suffix) * grid[i][j] % kMod;
+            }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    vector<vector<int>> grid = {{1, 2}, {3, 4}};
+    vector<vector<int>> ans = sol.constructProductMatrix(grid);
+
+    for (const vector<int> &row : ans)
+    {
+        for (const int num : row)
+            cout << num << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
+
+/*
+TC: O(m*n) -> where m is the number of rows and n is the number of columns in the grid.
+SC: O(m*n) -> for the answer matrix and the prefix product vector.
+*/
