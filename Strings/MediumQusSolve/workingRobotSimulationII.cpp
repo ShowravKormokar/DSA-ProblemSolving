7cd8df60d7c7018bfe0ae9d6c43538a89cf91f9b
ShowravKormokar <showravkormokar354@gmail.com>
2026-04-07 23:16:23 +0600

    Solved: Leetcode problem 2069 -> Walking Robot Simulation II

---
+++ Strings/MediumQusSolve/workingRobotSimulationII.cpp
@@ -0,0 +1,67 @@
+// Leetcode problem 2069 -> Walking Robot Simulation II
+#include <bits/stdc++.h>
+using namespace std;
+
+class Robot
+{
+public:
+    Robot(int width, int height)
+    {
+        pos.push_back({{0, 0}, "South"});
+        for (int i = 1; i < width; ++i)
+            pos.push_back({{i, 0}, "East"});
+        for (int j = 1; j < height; ++j)
+            pos.push_back({{width - 1, j}, "North"});
+        for (int i = width - 2; i >= 0; --i)
+            pos.push_back({{i, height - 1}, "West"});
+        for (int j = height - 2; j > 0; --j)
+            pos.push_back({{0, j}, "South"});
+    }
+
+    void step(int num)
+    {
+        isOrigin = false;
+        i = (i + num) % pos.size();
+    }
+
+    vector<int> getPos()
+    {
+        return pos[i].first;
+    }
+
+    string getDir()
+    {
+        return isOrigin ? "East" : pos[i].second;
+    }
+
+private:
+    bool isOrigin = true;
+    int i = 0;
+    vector<pair<vector<int>, string>> pos;
+};
+
+int main()
+{
+    Robot robot(6, 3);
+    robot.step(2);
+    vector<int> pos = robot.getPos();
+    string dir = robot.getDir();
+    cout << "Position: [" << pos[0] << ", " << pos[1] << "], Direction: " << dir << endl;
+
+    robot.step(2);
+    pos = robot.getPos();
+    dir = robot.getDir();
+    cout << "Position: [" << pos[0] << ", " << pos[1] << "], Direction: " << dir << endl;
+
+    robot.step(2);
+    pos = robot.getPos();
+    dir = robot.getDir();
+    cout << "Position: [" << pos[0] << ", " << pos[1] << "], Direction: " << dir << endl;
+
+    return 0;
+}
+
+/*
+TC: O(1) for step, getPos and getDir functions. O(w + h) for constructor where w is width and h is height.
+SC: O(w + h) for storing the positions and directions in the pos vector.
+*/
